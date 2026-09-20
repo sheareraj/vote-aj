768462305668856859779bbc9afa73f9aa0dfc5f
sheareraj <shearer.aj@gmail.com>
2026-09-20 11:54:42 -0400

    updated and added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-// https://astro.build/config
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
