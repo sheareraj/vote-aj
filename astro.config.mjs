a050154e84fdeea78ebbf9cdfdaeceb578ceaf55
sheareraj <shearer.aj@gmail.com>
2026-09-21 11:11:51 -0400

    update redirect (attempt 16)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
 import { defineConfig } from 'astro/config';
+
 import react from '@astrojs/react';
+
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+
   integrations: [react()],
-  adapter: vercel()
+
+  adapter: vercel(),
+
+  redirects: {
+    '/dtc': {
+      status: 302,
+      destination: 'https://www.youtube.com/playlist?list=PLfzsxh8e8t4A'
+    },
+
+    '/DTC': {
+      status: 302,
+      destination: 'https://www.youtube.com/playlist?list=PLfzsxh8e8t4A'
+    }
+  }
+
 });
